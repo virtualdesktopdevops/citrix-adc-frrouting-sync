e691e10457b949b44a7a62f1ad40de140862fa1a
matt6697 <32440697+matt6697@users.noreply.github.com>
2020-09-01 19:08:52 +0200

    Added configuration file parameter Removed static conf/config ini file and replaced it with --config parameter Checked configuration keys before calling the sync function to properly handle missing parameter scenario

--- run.py
+++ run.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 
 import configparser
+import sys
+import getopt
 from citrix_adc_frrouting_sync import citrix_adc_frrouting_sync
 
-# Read local file `config.ini`.
-config = configparser.ConfigParser()
-config.read('conf/config.ini')
+def main(argv):
+  configfile = ''
+  try:
+    opts, args = getopt.getopt(argv,"hi:o:",["config="])
+  except getopt.GetoptError:
+    print('run.py --config <configfile>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      print('run.py --config <configfile>')
+      sys.exit()
+    elif opt in ("-c", "--config"):
+      configfile = arg
+ 
+  # Read configuration file
+  config = configparser.ConfigParser()
+  config.read(configfile)
 
-# Run a test server.
-citrix_adc_frrouting_sync.Sync(config['ADC']['NITRO_URL'], config['ADC']['LOGIN'], config['ADC']['PASSWORD'])
+  # Run sync
+  if config.has_option('ADC', 'NITRO_URL') and config.has_option('ADC', 'PASSWORD') and config.has_option('ADC', 'LOGIN'):
+    citrix_adc_frrouting_sync.Sync(config['ADC']['NITRO_URL'], config['ADC']['LOGIN'], config['ADC']['PASSWORD'])
+  else:
+    print('Missing at least one of the NITRO_URL, LOGIN, or PASSWORD configuration parameters')
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
