1dc581e129539af6b9e43425738ea6f6ce415e4f
adjdevapps <adjdevapps@homeworkdomain.com>
2020-09-01 21:55:08 +0200

    Passed nexthop ip to sync function and implemented real vtysh commands

--- citrix_adc_frrouting_sync/citrix_adc_frrouting_sync.py
+++ citrix_adc_frrouting_sync/citrix_adc_frrouting_sync.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import citrix_adc_frrouting_sync.nitro as nitro
+import os
 
 class Sync():
-  def __init__(self, url, username, password):
+  def __init__(self, url, username, password, nexthop):
     # start nitro client
     nitro_client = nitro.NitroClient(url, username, password)
 
@@ -19,16 +20,21 @@ class Sync():
         params="args=type:VIP"
     )
 
-    try: 
+    try:
       #Check HTTP response code and parse only if result contains data.
       for vip in result.json()['nsip']:
         print(vip)
         # IF state = ENABLED & hostroute=ENABLED & vipvipvsrvrrhiactiveupcount > 0 => publish route
         if vip['state'] == 'ENABLED' and vip['hostroute'] == 'ENABLED':
-          print("vtysh -c 'configure terminal' -c 'ip route " + vip['ipaddress'] + "255.255.255.255 " + config['ADC']['CONTAINER_IP_ADDRESS'] +"'")
+          cmd = "vtysh -c 'configure terminal' -c 'ip route " + vip['ipaddress'] + " 255.255.255.255 " + nexthop +"'"
+          os.system(cmd)
+          print(cmd)
         # Else remove route. This method ensures that disabled VIP or unconfigured hostroutes are also removed from routing table
         else:
+          cmd = "vtysh -c 'configure terminal' -c 'no ip route " + vip['ipaddress'] + " 255.255.255.255 " + nexthop +"'"
+          os.system(cmd)
           print("Removing VIP "+ vip['ipaddress'] + " from routing table")
+          print(cmd)
 
         # Make sure that no deleted VIP remains in frrouting by comparing static routes list with RHI enabled VIP list
 
--- run.py
+++ run.py
@@ -18,14 +18,14 @@ def main(argv):
       sys.exit()
     elif opt in ("-c", "--config"):
       configfile = arg
- 
+
   # Read configuration file
   config = configparser.ConfigParser()
   config.read(configfile)
 
   # Run sync
-  if config.has_option('ADC', 'NITRO_URL') and config.has_option('ADC', 'PASSWORD') and config.has_option('ADC', 'LOGIN'):
-    citrix_adc_frrouting_sync.Sync(config['ADC']['NITRO_URL'], config['ADC']['LOGIN'], config['ADC']['PASSWORD'])
+  if config.has_option('ADC', 'NITRO_URL') and config.has_option('ADC', 'PASSWORD') and config.has_option('ADC', 'LOGIN') and config.has_option('ADC', 'CONTAINER_IP_ADDRESS'):
+    citrix_adc_frrouting_sync.Sync(config['ADC']['NITRO_URL'], config['ADC']['LOGIN'], config['ADC']['PASSWORD'], config['ADC']['CONTAINER_IP_ADDRESS'])
   else:
     print('Missing at least one of the NITRO_URL, LOGIN, or PASSWORD configuration parameters')
 
