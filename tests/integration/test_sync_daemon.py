dedce0b3b85ec1f36ccfb100a7ff1038baef711e
matt6697 <32440697+matt6697@users.noreply.github.com>
2020-09-30 18:58:16 +0200

    Corrected wrong test description and testing multiple VIP injection

--- tests/integration/test_sync_daemon.py
+++ tests/integration/test_sync_daemon.py
@@ -30,7 +30,7 @@ class TestSyncDaemon(TestCase):
         static_routes = frrclient.get_routes_with_tag(99)
         assert list(static_routes)[0] == '10.1.2.3/32'
 
-    # Route should be withdrawn when service goes up
+    # Route should be withdrawn when service goes down
     def test_disable_citrix_adc_service(self):
         os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'disable service google_ssl_svc'")
         time.sleep(10)
@@ -53,3 +53,31 @@ class TestSyncDaemon(TestCase):
         frrclient = citrix_adc_frrouting.frrouting.FrroutingClient()
         static_routes = frrclient.get_routes_with_tag(99)
         self.assertFalse(static_routes)
+
+    # Multiple routes have to be advertised when multiple VIPs are configured
+    def test_add_multiple_citrix_adc_lb_vservers(self):
+        # Restart Citrix ADC CPX and wait for initialization
+        os.system("docker-compose -f docker-compose.ci.yml up -d")
+        time.sleep(90)
+
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add service google_ssl_svc 216.58.206.227 SSL 443'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add ns ip 10.1.2.3 255.255.255.255 -type VIP -hostroute ENABLED'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add lb vserver google_http_vsrv HTTP 10.1.2.3 80'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'bind lb vserver google_http_vsrv google_ssl_svc'")
+
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add service facebook_ssl_svc 179.60.195.36 SSL 443'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add ns ip 11.1.2.3 255.255.255.255 -type VIP -hostroute ENABLED'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add lb vserver facebook_http_vsrv HTTP 11.1.2.3 80'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'bind lb vserver facebook_http_vsrv facebook_ssl_svc'")
+
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add service twitter_ssl_svc 179.60.195.36 SSL 443'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add ns ip 12.1.2.3 255.255.255.255 -type VIP -hostroute ENABLED'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'add lb vserver twitter_http_vsrv HTTP 12.1.2.3 80'")
+        os.system("docker exec -i citrix-adc-frrouting-sync_cpx_130_1 cli_script.sh 'bind lb vserver twitter_http_vsrv twitter_ssl_svc'")
+
+        time.sleep(10)
+        frrclient = citrix_adc_frrouting.frrouting.FrroutingClient()
+        static_routes = frrclient.get_routes_with_tag(99)
+        assert list(static_routes)[0] == '10.1.2.3/32'
+        assert list(static_routes)[1] == '11.1.2.3/32'
+        assert list(static_routes)[2] == '12.1.2.3/32'
