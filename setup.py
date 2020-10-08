a6c396135b73649fc7afc208bb7a85613cfea7b3
matt6697 <32440697+matt6697@users.noreply.github.com>
2020-10-08 19:13:26 +0200

    Added debian packaging with python3-stdeb util

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/virtualdesktopdevops/citrix-adc-frrouting-sync",
     packages=["citrix_adc_frrouting"],
+    scripts=['citrixadcfrrsync.py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
