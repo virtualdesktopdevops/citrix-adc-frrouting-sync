74cdab7c84c88f5d6a8ffba4aa57b45d100a234d
matt6697 <32440697+matt6697@users.noreply.github.com>
2020-10-21 08:19:36 +0200

    Packaged sample configuration file for systemd service and added python module requirements as debian package dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="VirtualDesktopDevops",
     author_email="author@example.com",
-    description="Python application to advertise RHI enabled Citrix ADC CPX virtual IP addresses in the network using Frrouting router.",
+    description="Advertise RHI enabled Citrix ADC CPX VIP using Frrouting",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/virtualdesktopdevops/citrix-adc-frrouting-sync",
@@ -20,4 +20,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
+    install_requires=[
+          'python-daemon',
+          'requests'
+      ],
 )
